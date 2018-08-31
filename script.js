f39de997983285e32320e666fe692fe43310afa8
Misa Jokisalo <misa.jokisalo@gmail.com>
2018-08-31 15:32:33 +0300

    Theme switcher

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var isOpen = false;
+var isOpen = true;
 
 function toggleNativeSelect() {
   if (isOpen) {
@@ -10,6 +10,25 @@ function toggleNativeSelect() {
   }
 }
 
+
+function setDarkTheme() {
+  // Remove active class from light theme
+  document.querySelector('.color-select.light').classList.remove('active');
+  // Hide light themed content and show dark
+  document.querySelector('.fs-select-container.light').setAttribute('style', 'display: none;');
+  document.querySelector('.fs-select-container.dark').setAttribute('style', 'display: block;');
+}
+
+
+function setLightTheme() {
+  // Remove active class from dark theme
+  document.querySelector('.color-select.dark').classList.remove('active');
+  // Hide dark themed content and show light
+  document.querySelector('.fs-select-container.dark').setAttribute('style', 'display: none;');
+  document.querySelector('.fs-select-container.light').setAttribute('style', 'display: block;');
+}
+
+
 function handleColorSelect(event) {
   // Event is a mouse click event. We want to manipulate its target.
   var elem = event.target;
@@ -18,30 +37,34 @@ function handleColorSelect(event) {
   if (elem.classList.contains('active')) return false;
 
   if (elem.classList.contains('dark')) {
-    // Remove active class from light theme
-    document.querySelector('.color-select.light').classList.remove('active');
-    // Hide light themed content and show dark
-    document.querySelector('.fs-select-container.light').setAttribute('style', 'display: none;');
-    document.querySelector('.fs-select-container.dark').setAttribute('style', 'display: block;');
+    setDarkTheme();
   } else {
-    // Remove active class from dark theme
-    document.querySelector('.color-select.dark').classList.remove('active');
-    // Hide dark themed content and show light
-    document.querySelector('.fs-select-container.dark').setAttribute('style', 'display: none;');
-    document.querySelector('.fs-select-container.light').setAttribute('style', 'display: block;');
+    setLightTheme();
   }
   // Set new element as active
   elem.classList.add('active');
 }
+
 
 window.docReady(function () {
   // Add debug toggle listener to h1
   var h1 = document.getElementsByTagName('h1')[0];
   h1.addEventListener('click', toggleNativeSelect);
 
+  // Hide debug thing by default
+  toggleNativeSelect();
+
   // Add color change toggle listeners to color-selects
   var cs = document.getElementsByClassName('color-select');
   for (var i = 0; i < cs.length; i++) {
     cs[i].addEventListener('click', handleColorSelect);
+  }
+
+  // Update color selection once
+  var theme = document.querySelector('.color-select.active');
+  if (theme.classList.contains('dark')) {
+    setDarkTheme();
+  } else {
+    setLightTheme();
   }
 });
